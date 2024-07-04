22e800b7b408393d361dbc1c4ec86062f2b7b9cb
dcdnc <kuunii06@gmail.com>
2024-07-04 12:03:30 +0700

    app fix

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,23 @@
 package app
 
+import (
+	"log"
+
+	"github.com/lunn06/smart-toy-backend/internal/config"
+	"github.com/lunn06/smart-toy-backend/internal/services"
+	"github.com/lunn06/video-hosting/internal/database"
+)
+
+func init() {
+	config.Init()
+	database.Init()
+	//initializers.ParseConfig()
+	//initializers.ConnectToDB()
+}
+
 func Run() {
-	println("Hello world")
+	r := services.SetupRouter()
+
+	onlyPortAddress := ":" + config.CFG.HTTPServer.Port
+	log.Fatal(r.Run(onlyPortAddress))
 }
